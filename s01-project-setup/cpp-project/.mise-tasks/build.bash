8972d519b13a104c1c2e7306948f3186a78f2926
dezlymacauley <dezlymacauley@gmail.com>
2026-09-10 20:57:07 +0200

    Added error handling to 'mise build' command

--- s01-project-setup/cpp-project/.mise-tasks/build.bash
+++ s01-project-setup/cpp-project/.mise-tasks/build.bash
@@ -3,5 +3,24 @@
 #MISE description="👷 Build the project"
 #MISE quiet=true
 
-cmake -B build -G Ninja
-cmake --build build
+#______________________________________________________________________________
+
+# STEP: 1 => Generate the build instructions
+
+if ! cmake -B build -G Ninja; then
+    printf "\n%s\n\n"  '❌ Failed to generate build instructions'
+    exit 1
+fi
+
+printf "\n%s\n\n" '✅ Build instructions generated'
+#______________________________________________________________________________
+
+# STEP: 2 => Generate the build instructions
+
+if ! cmake --build build; then
+    printf "\n%s\n\n"  '❌ Failed to build project'
+    exit 1
+fi
+
+printf "\n%s\n\n" '✅ Project built'
+#______________________________________________________________________________
