765917013d2e98b4c364dda9ea5fe621e906b625
dezlymacauley <dezlymacauley@gmail.com>
2026-09-10 21:19:43 +0200

    Improved 'mise dev' command

--- s01-project-setup/cpp-project/.mise-tasks/dev.bash
+++ s01-project-setup/cpp-project/.mise-tasks/dev.bash
@@ -3,4 +3,31 @@
 #MISE description="🚀 Run the project"
 #MISE quiet=true
 
+#______________________________________________________________________________
+
+# STEP: 1 => Generate the build instructions
+
+# If there was an error generating the build instructions, 
+# it will be displayed. 
+if ! build_instruction_error_message=$(cmake -B build -G Ninja 2>&1); then
+    printf "\n%s\n\n" '❌ Failed to generate build instructions'
+    printf "%s\n" "$build_instruction_error_message"
+    exit 1
+fi
+#______________________________________________________________________________
+
+# STEP: 2 => Build the project
+
+# If there was an error building the project,
+# it will be displayed. 
+if ! build_output_error_messages=$(cmake --build build 2>&1); then
+    printf "\n%s\n\n" '❌ Failed to build project'
+    printf "%s\n" "$build_output_error_messages"
+    exit 1
+fi
+#______________________________________________________________________________
+
+# STEP: 3 => Run the project
+
 ./build/cpp_project
+#______________________________________________________________________________
--- s01-project-setup/cpp-project/src/main.cpp
+++ s01-project-setup/cpp-project/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main() {
+zzzint main() {
     std::cout << "\nC++ Project\n\n";
     return 0;
 }
