d0a074fd44e575e00e9d07da606096e7908186b0
dezlymacauley <dezlymacauley@gmail.com>
2026-09-15 15:18:47 +0200

    Finally working

--- d02-core-syntax/d02-multi-file-programs/d04-directories-that-contain-sub-directories-v2/app/main.cpp
+++ d02-core-syntax/d02-multi-file-programs/d04-directories-that-contain-sub-directories-v2/app/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
 
-    print_cmake_explained();
+    print_hello_world();
 
     return 0;
 }
--- d02-core-syntax/d02-multi-file-programs/d04-directories-that-contain-sub-directories-v2/include/my-lib/my_lib.hpp
+++ d02-core-syntax/d02-multi-file-programs/d04-directories-that-contain-sub-directories-v2/include/my-lib/my_lib.hpp
@@ -1,3 +1,3 @@
 #pragma once
 
-void print_cmake_explained();
+void print_hello_world();
